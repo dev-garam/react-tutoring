444fd05550c26bfdd99dbefee9b026de2c0b37c9
김가람 <feelsky665@naver.com>
2020-06-12 14:42:51 +0900

    react-calendar 사용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Calendar from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        date: new Date(),
+        birthday: '',
+    }
+
+    formatDate(date) {
+        let d = new Date(date),
+            month = '' + (d.getMonth() + 1),
+            day = '' + d.getDate(),
+            year = d.getFullYear()
+
+        if (month.length < 2) month = '0' + month
+        if (day.length < 2) day = '0' + day
+
+        return [year, month, day].join('-')
+    }
+
+    componentDidMount() {
+        const birthday = this.formatDate(this.state.date)
+        this.setState({
+            birthday,
+        })
+    }
+
+    onChange = (date) => {
+        const birthday = this.formatDate(date)
+        this.setState({ date, birthday })
+    }
+
+    render() {
+        return (
+            <div>
+                <button>버튼 1</button>
+                <Calendar
+                    className="calendar"
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
+                <button>버튼 2</button>
+                <button>버튼 3</button>
+                <button>버튼 4</button>
+                <button>버튼 5</button>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
